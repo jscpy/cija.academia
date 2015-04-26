a3c590d6a41ed763080adf3096152ec998e8f1ac
jscpy <chuy.sc@hotmail.com>
2015-04-26 01:21:41 -0500

    mensaje

---
+++ academia/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('academia', '0008_auto_20150426_0517'),
+        ('academia', '0008_auto_20150426_0531'),
+    ]
+
+    operations = [
+    ]
