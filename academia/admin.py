760bdc46fadac10e4aa68e2dbfd2a60712597993
jscpy <chuy.sc@hotmail.com>
2015-04-25 21:40:21 -0500

    mensaje

--- academia/admin.py
+++ academia/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from django_markdown.admin import MarkdownModelAdmin, AdminMarkdownWidget
+from django_markdown.admin import MarkdownModelAdmin
 from django.db.models import TextField
 from academia.models import Capitulo, Recurso, Usuario, Post
-
-class PostAdmin(MarkdownModelAdmin):
-	formfield_overrides = {TextField: {'widget': AdminMarkdownWidget}}
 
 admin.site.register(Capitulo)
 admin.site.register(Recurso)
 admin.site.register(Usuario)
-admin.site.register(Post, PostAdmin)
+admin.site.register(Post,MarkdownModelAdmin)
